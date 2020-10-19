3f273163883a4500cbb07af221709a7c5fac7668
ctrlaltdelete44 <daviesanthonye1234@gmail.com>
2020-10-19 12:35:30 +0100

    Added rudimentary styles and default sheets to the project

---
+++ assets/js/application.js
@@ -0,0 +1,33 @@
+$(window).on('load', () => {
+  $('#sayHello').click(() => {
+    let name = $('#name').val()
+    $('#sayHello_result').html(`Hello ${name}!`)
+  });
+
+  $('#checkNumber').click(() => {
+    let className, message;
+    let number = $('#number').val()
+
+    $('#numberMessage').removeClass('less greater')
+
+    if (number < 10) {
+      className = 'less';
+      message = 'less than';
+    } else {
+      className = 'greater';
+      message = 'greater than or equal to';
+    }
+
+    $('#numberMessage').removeClass('hidden')
+    $('#numberMessage').addClass(className)
+
+    $('#numberMessageText').html(`Number is ${message} 10`)
+  });
+
+  $('#findUnique').click(() => {
+    let entered = $('#uniqueNumbers').val()
+    let uniqueNumbers = new Set(entered.split(','))
+
+    $('#uniqueMessageText').html([...uniqueNumbers].join(', '))
+  });
+});
