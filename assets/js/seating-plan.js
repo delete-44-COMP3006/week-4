de8cc5790ada19cf2336ad843e061e69e5be17fb
ctrlaltdelete44 <daviesanthonye1234@gmail.com>
2020-10-20 15:03:39 +0100

    Added code for booking chairs

--- assets/js/seating-plan.js
+++ assets/js/seating-plan.js
@@ -1,3 +1,17 @@
 $(window).on('load', () => {
+  $('.seat').click(function() {
+    $(this).toggleClass('booked')
 
+    if ($(this).hasClass('booked')) {
+      localStorage.setItem(this.id, 1)
+    } else {
+      localStorage.removeItem(this.id)
+    }
+  })
+
+  $('.seat').each(function() {
+    if(localStorage.getItem(this.id) !== null) {
+      $(this).addClass('booked')
+    }
+  })
 });
